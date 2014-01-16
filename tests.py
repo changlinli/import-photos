688e6b4dada64ee52d114e7a2885df079be532a8
Changlin Li <changlin@localhost.localdomain>
2014-01-16 03:34:28 -0500

    Added new test case for copying
    Added new test case which checks copying of pictures with no flags under
    side_effects_copy_file_with_flags.

--- tests.py
+++ tests.py
@@ -1,11 +1,31 @@
 import unittest
 import import_photos
+import os
+import shutil
+
+CURRENT_FILE_DIR = os.path.dirname(os.path.realpath(__file__))
+SAMPLE_PHOTO_LOCATION = os.path.join(CURRENT_FILE_DIR, "test_data/IMG_0028.CR2")
+SAMPLE_PHOTO_FILENAME = "IMG_0028.CR2"
 
 class TestReadEXIFData(unittest.TestCase):
     def setUp(self):
-        sample_photo = "test_data/IMG_0028.CR2"
+        sample_photo = SAMPLE_PHOTO_LOCATION
         self.photo_fp = open(sample_photo, 'rb')
 
     def test_get_tags(self):
         date_as_list = import_photos.get_img_date(self.photo_fp)
         self.assertEqual(['2014', '01', '01'], date_as_list)
+
+class TestCopyPictures(unittest.TestCase):
+    def setUp(self):
+        self.destination = os.path.join(CURRENT_FILE_DIR, SAMPLE_PHOTO_FILENAME)
+    def test_copy_with_no_flags(self):
+        import_photos.side_effects_copy_file_with_flags(SAMPLE_PHOTO_LOCATION,
+                                                        self.destination)
+        isfile_flag = os.path.isfile(self.destination)
+        self.assertTrue(isfile_flag)
+    def tearDown(self):
+        try:
+            os.remove(self.destination)
+        except IOError:
+            pass
